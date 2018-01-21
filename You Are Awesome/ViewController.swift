165a5a72f48c89c9b287922e8b8bc1d3c723b9ae
Lurein Perera <brickseason@gmail.com>
2018-01-20 22:12:10 -0500

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Enoka Jayamanne on 1/20/18.
+//  Copyright © 2018 Lurein Perera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
