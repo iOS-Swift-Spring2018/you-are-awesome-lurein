efa42234eb3a1835b95a89757b80058f77523aea
Lurein Perera <brickseason@gmail.com>
2018-01-22 17:19:21 -0500

    Initial Commit

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var messageButton: UIButton!
+    
+    var index = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+       
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Legendary!",
+                        "You Make the Mirror Look Good!",
+                        "You Are as Fine as a Sharpie!",
+                        "I'd Choose You Over Siri!"]
+        
+        if index == messages.count-1 {
+            index = 0
+        } else{
+            index += 1
+        }
+        messageLabel.text = messages[index]
+        /*
+        let message1 = "You Are Awesome!"
+        let message2 = "You Are Great!"
+        let message3 = "You Are Legendary!"
+        if messageLabel.text == message1{
+            messageLabel.text = message2
+        } else if messageLabel.text == message2 {
+            messageLabel.text = message3
+        } else {
+            messageLabel.text = message1
+        } */
+    }
 
-
+    
 }
 
